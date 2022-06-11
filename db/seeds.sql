7d2aa81086695d05e015a03d01336ce8f4b68311
Alexb <acbird.ab@gmail.com>
2022-06-11 16:39:13 -0600

    fixed tables

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,27 +1,27 @@
 INSERT INTO department(name)
-VALUES('Sales'),
-VALUES('Engineering'),
-VALUES('Finance'),
-VALUES('Legal');
+VALUES("Sales"),
+("Engineering"),
+("Finance"),
+("Legal");
 
 
 INSERT INTO role (title, salary, department_id)
-VALUES('Sales Lead', 100000, 1),
-VALUES('Salesperson',  80000, 1),
-VALUES('Lead Engineer', 150000, 2),
-VALUES('Software Engineer', 120000, 2),
-VALUES('Account Manager', 160000, 3),
-VALUES('Accountant', 125000, 3),
-VALUES('Legal Team Lead',  250000, 4);
-VALUES('Lawyer',  190000, 4);
+VALUES("Sales Lead", 100000, 1),
+("Salesperson",  80000, 1),
+("Lead Engineer", 150000, 2),
+("Software Engineer", 120000, 2),
+("Account Manager", 160000, 3),
+("Accountant", 125000, 3),
+("Legal Team Lead",  250000, 4),
+("Lawyer",  190000, 4);
 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES('John', 'Doe', 1, NULL),
-VALUES('Mike', 'Chan', 2, 1),
-VALUES('Ashely', 'Rodriguez', 3, NULL),
-VALUES('Kevin', 'Tupik', 4, 3),
-VALUES('Kunal', 'Singh', 5, NULL),
-VALUES('Malia', 'Brown', 6, 5),
-VALUES('Sarah', 'Lourd', 7, NULL),
-VALUES('Tom', 'Allen', 8, 7);
+VALUES("John", "Doe", 1, NULL),
+("Mike", "Chan", 2, 1),
+("Ashely", "Rodriguez", 3, NULL),
+("Kevin", "Tupik", 4, 3),
+("Kunal", "Singh", 5, NULL),
+("Malia", "Brown", 6, 5),
+("Sarah", "Lourd", 7, NULL),
+("Tom", "Allen", 8, 7);
