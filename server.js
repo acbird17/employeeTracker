6793d7dac4989862d0d5744d0360ced8ab76f58e
Alexb <acbird.ab@gmail.com>
2022-06-11 16:52:50 -0600

    do i need express?

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 const sequelize = require("./db/connection");
+const express = require("express");
+const routes = require("./routes");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
